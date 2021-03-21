d8a51294221004114a9014c15b4b1344b88afb6c
devunderslash <devunderslash@hotmail.com>
2021-03-21 21:24:02 +0000

    key updates
    updating author

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,4 @@ provider "aws" {
   # profile                 = "development"
 }
 
-resource "tls_private_key" "this" {
-  algorithm = "RSA"
-}
-
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
-
-  key_name   = "terra-jen-key"
-  public_key = tls_private_key.this.public_key_openssh
-}
 
